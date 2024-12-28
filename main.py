c5eb2f325204f209f981441fb8fa66c2f8478001
Cole Juracek <therealjuracek@gmail.com>
2024-12-28 13:58:04 -0700

    Add column to streamlit layout for pearl 3

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import click
 import streamlit as st
 
 from life4.ddr import DDRDataset
-from life4.rank_requirements.a20_plus import pearl_1, pearl_2
+from life4.rank_requirements.a20_plus import pearl_1, pearl_2, pearl_3
 
 
 @click.command()
 @click.option("--data-path", required=True, type=str, help="Path to data file")
 def main(data_path: str):
     data = DDRDataset(data_path)
-    col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
     with col1:
         pearl_1.visualize(data)
     with col2:
         pearl_2.visualize(data)
+    with col3:
+        pearl_3.visualize(data)
 
 
 if __name__ == "__main__":
