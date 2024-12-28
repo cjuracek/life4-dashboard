8871d2f463d465e23017dbe881792ae33d22eb2a
Cole Juracek <therealjuracek@gmail.com>
2024-12-28 13:53:06 -0700

    Viz pearl 1 and 2 in a column layout

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import click
+import streamlit as st
 
 from life4.ddr import DDRDataset
-from life4.rank_requirements.a20_plus import pearl_1
+from life4.rank_requirements.a20_plus import pearl_1, pearl_2
 
 
 @click.command()
 @click.option("--data-path", required=True, type=str, help="Path to data file")
 def main(data_path: str):
     data = DDRDataset(data_path)
-    pearl_1.visualize(data)
+    col1, col2 = st.columns(2)
+    with col1:
+        pearl_1.visualize(data)
+    with col2:
+        pearl_2.visualize(data)
 
 
 if __name__ == "__main__":
